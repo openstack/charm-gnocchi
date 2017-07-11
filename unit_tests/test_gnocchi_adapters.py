b85c2266f648ca88ce8e01af2ff647fbdfd61c3a
James Page <james.page@ubuntu.com>
2017-07-11 11:34:29 +0100

    Add unit tests for adapters

---
+++ unit_tests/test_gnocchi_adapters.py
@@ -0,0 +1,51 @@
+# Copyright 2017 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import mock
+
+import charms_openstack.test_utils as test_utils
+
+import charm.openstack.gnocchi as gnocchi
+
+
+class TestAdapters(test_utils.PatchHelper):
+
+    _mons = [
+        '1.2.3.4:123',
+        '1.2.3.5:123',
+        '1.2.3.6:123',
+    ]
+
+    _memcache = ['1.2.3.4', '1.2.3.5']
+
+    def test_storage_ceph(self):
+        adapter = gnocchi.StorageCephRelationAdapter()
+        adapter.relation = mock.MagicMock()
+        adapter.relation.mon_hosts.return_value = self._mons
+        self.assertEqual(adapter.monitors,
+                         ','.join(self._mons))
+        adapter.relation.mon_hosts.return_value = []
+        self.assertEqual(adapter.monitors, None)
+
+    def test_memcache(self):
+        adapter = gnocchi.MemcacheRelationAdapter()
+        adapter.relation = mock.MagicMock()
+        adapter.relation.memcache_hosts.return_value = self._memcache
+        self.assertEqual(adapter.url,
+                         "memcached://1.2.3.4:11211?timeout=5")
+        adapter.relation.memcache_hosts.return_value = []
+        self.assertEqual(adapter.url, None)
