b0a1551e56f2a540942cbd745fa4658057100c5e
David Ames <david.ames@canonical.com>
2018-02-26 08:55:14 -0800

    Explicitly call configure_ssl
    Although, layer_openstack_api calls configure_ssl the ordering is
    indeterminate. Explicitly call configure_ssl in the charm layer before
    services are enabled to avoid attempting to start apache2 before
    certificates are available.
    
    Change-Id: Icb40bbcf4ca920be202584e9e25ffd1c2b9c8d61
    Partial-Bug: #1750915

--- src/reactive/gnocchi_handlers.py
+++ src/reactive/gnocchi_handlers.py
@@ -50,6 +50,7 @@ def render_config(*args):
     """
     with charm.provide_charm_instance() as charm_class:
         charm_class.upgrade_if_available(args)
+        charm_class.configure_ssl()
         charm_class.enable_services()
         charm_class.render_with_interfaces(args)
         charm_class.enable_webserver_site()
